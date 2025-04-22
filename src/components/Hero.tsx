db799a61b60ca36f90be542260ac60d7513a53a2
unknown <USER-1@mic-8.ad.dgu.ru>
2025-04-22 10:52:39 +0300

    правки по главной странице

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 const Hero = () => {
 	return (
 		<section className='bg-victory-dark-blue text-white h-full w-full flex items-center relative overflow-hidden'>
@@ -17,11 +19,13 @@ const Hero = () => {
 			</div>
 
 			<div className='container mx-auto px-4 relative z-10'>
-				<div className='text-center max-w-3xl mx-auto'>
+				<div className='text-center max-w-2jxl mx-auto'>
 					<div className='text-4xl md:text-6xl font-bold mb-6 leading-tight'>
-						<h1 className='text-3d'>Память о подвиге</h1>
-						<h1 className='text-3d'>героев Великой</h1>
-						<h1 className='text-3d'>Отечественной Войны</h1>
+						<div className='flex flex-col items-start'>
+							<h1 className='text-3d whitespace-nowrap'>Память о подвиге</h1>
+							<h1 className='text-3d whitespace-nowrap'>героев Великой</h1>
+							<h1 className='text-3d whitespace-nowrap'>Отечественной Войны</h1>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -31,20 +35,18 @@ const Hero = () => {
 				<blockquote className="relative max-w-3xl bg-black-opacity-80 rounded-md py-4 px-6 text-gray-300 text-left pointer-events-auto shadow-lg border-l-4 border-blue-400 before:content-[''] before:absolute before:inset-0 before:rounded-md before:backdrop-blur-lg before:-z-10">
 					<p className='italic leading-relaxed mb-2'>
 						Товарищи далеких дней моих,
-						<br />
-						Ровесники, прожившие так мало!..
+						ровесники, прожившие так мало!..
 						<br />
 						Наверное, остался я в живых,
-						<br />
-						Чтоб память на земле не умирала.
+						чтоб память на земле не умирала.
 					</p>
 					<p className='italic leading-relaxed mb-2'>
-						На поле боя павшие друзья —<br />
-						Вас было много, страстно жизнь любивших.
+						На поле боя павшие друзья —
+						вас было много, страстно жизнь любивших.
 						<br />
 						Я ведаю: в живых остался я,
-						<br />
-						Чтоб рассказать о вас, так мало живших.
+						
+						чтоб рассказать о вас, так мало живших.
 					</p>
 					<footer className='mt-2 text-sm text-gray-400'>
 						— Расул Гамзатов
